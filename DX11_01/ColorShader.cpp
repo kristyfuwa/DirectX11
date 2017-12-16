080f2afb780376c17e2c23b21f49694cbc371652
wanghuan <kristyfuwa@163.com>
2017-12-16 18:01:26 +0800

    000

--- DX11_01/ColorShader.cpp
+++ DX11_01/ColorShader.cpp
@@ -39,29 +39,45 @@ void ColorShader::Shutdown()
 void ColorShader::ShutdownShader()
 {
 	// 释放常量缓冲
-		if (m_pMatrixBuffer)
-		{
-			m_pMatrixBuffer->Release();
-			m_pMatrixBuffer = 0;
-		}
-		//释放顶点布局
-		if (m_pLayout)
-		{
-			m_pLayout->Release();
-			m_pLayout = 0;
-		}
-		// 释放ps .
-		if (m_pPixelShader)
-		{
-			m_pPixelShader->Release();
-			m_pPixelShader = 0;
-		}
+	if (m_pMatrixBuffer)
+	{
+		m_pMatrixBuffer->Release();
+		m_pMatrixBuffer = 0;
+	}
+	//释放顶点布局
+	if (m_pLayout)
+	{
+		m_pLayout->Release();
+		m_pLayout = 0;
+	}
+	// 释放ps .
+	if (m_pPixelShader)
+	{
+		m_pPixelShader->Release();
+		m_pPixelShader = 0;
+	}
 
-		// 释放vs
-		if (m_pVertexShader)
-		{
-			m_pVertexShader->Release();
-			m_pVertexShader = 0;
-			
-		}
+	// 释放vs
+	if (m_pVertexShader)
+	{
+		m_pVertexShader->Release();
+		m_pVertexShader = 0;
+	}
+}
+
+bool ColorShader::Render(ID3D11DeviceContext* device, int indexCount, D3DXMATRIX worldMatrix, D3DXMATRIX viewMatrix, D3DXMATRIX projectMatrix)
+{
+	bool result;
+
+	//设置shader参数
+	result = SetShaderParameters(device, worldMatrix, viewMatrix, projectMatrix);
+	if (!result)
+		return false;
+}
+
+bool ColorShader::SetShaderParameters(ID3D11DeviceContext* device, D3DXMATRIX worldMatrix, D3DXMATRIX viewMatrix, D3DXMATRIX projectMatrix)
+{
+	HRESULT result;
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+
 }
